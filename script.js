e5dff3d654cd0760f4f42d0affcc7ed615e48569
Pedro Catalão <pedrocatalao@users.noreply.github.com>
2024-07-24 00:32:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ var dnaText = {
     dnaText.canvas.height = dnaText.H;
 
 
-    var cont = "ironik";
+    var cont = " ironik";
     setInterval(function(){
       dnaText.changeLetter(cont);
       dnaText.getPixels(dnaText.tmpCanvas, dnaText.tmpCtx);
